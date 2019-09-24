acacb1094b19905239ad463411905aeba3d3b7af
Timothy Ivaikin <tivaikin@fxcess.com>
2019-09-24 12:51:42 +0300

    Manager updated

--- rootcause/src/app/app.component.ts
+++ rootcause/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+class Task {
+  task: string;
+  w5h2?: string;
+  causes?: Array<Task>;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rootcause';
+  topTasks: Array<Task> = [
+    {
+      task: 'test1',
+      causes: []
+    },
+    {
+      task: 'test2',
+      causes: []
+    }
+  ];
 }
